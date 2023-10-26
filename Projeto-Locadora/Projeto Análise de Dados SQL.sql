6ac555b8611e67fc844cb97191e8def087732505
Klisman Gomes <76626836+klisman02@users.noreply.github.com>
2023-10-26 16:22:47 -0300

    Add files via upload

---
+++ Projeto-Locadora/Projeto Análise de Dados SQL.sql
@@ -0,0 +1,96 @@
+# --- ANÁLISES DE DADOS COM SQL --- #
+
+-- Agrupamentos
+-- Filtragem em agrupamentos
+-- Subqueries
+-- Criação de Views
+
+-- Lembrando das tabelas do banco de dados...
+
+SELECT * FROM alugueis;
+SELECT * FROM atores;
+SELECT * FROM atuacoes;
+SELECT * FROM clientes;
+SELECT * FROM filmes;
+
+# =======        PARTE 1:        =======#
+# =======  CRIANDO AGRUPAMENTOS  =======#
+
+-- CASE 1. Você deverá começar fazendo uma análise para descobrir o preço médio de aluguel dos filmes.
+SELECT AVG(preco_aluguel) AS "MÉDIA" FROM filmes;
+
+
+-- CASE 2. Agora que você sabe o preço médio para se alugar filmes na hashtagmovie, você deverá ir além na sua análise e descobrir qual é o preço médio para cada gênero de filme.
+SELECT 
+	genero AS GÊNERO, 
+    ROUND(AVG(preco_aluguel), 2) AS "PREÇO MÉDIO", COUNT(*) AS "TOTAL DE FILMES" FROM filmes 
+    GROUP BY genero;
+
+/*
+genero                   | preco_medio
+______________________________________
+Comédia                  | X
+Drama                    | Y
+Ficção e Fantasia        | Z
+Mistério e Suspense      | A
+Arte                     | B
+Animação                 | C
+Ação e Aventura          | D
+*/
+
+
+
+
+# =======              PARTE 2:               =======#
+# =======       FILTROS EM AGRUPAMENTOS       =======#
+
+-- Você deve alterar a consulta DO CASE 2 e considerar o seguinte cenário:
+
+-- CASE 3: Fazer a mesma análise, mas considerando apenas os filmes com ANO_LANCAMENTO igual a 2011.
+
+SELECT 
+	genero AS GÊNERO, 
+    ROUND(AVG(preco_aluguel), 2) AS "PREÇO MÉDIO", COUNT(*) AS "TOTAL DE FILMES" FROM filmes 
+    WHERE ano_lancamento = 2011 
+    GROUP BY genero; 
+    
+
+
+
+
+
+
+# =======                         PARTE 3:                           =======#
+# =======  SUBQUERIES: UTILIZANDO UM SELECT DENTRO DE OUTRO SELECT   =======#
+
+-- CASE 4. Você precisará fazer uma análise de desempenho dos alugueis. Para isso, uma análise comum é identificar quais aluguéis tiveram nota acima da média. Você seria capaz de fazer isso?
+
+SELECT ROUND(AVG(nota), 2) FROM alugueis; -- 7.94
+
+SELECT * FROM alugueis 
+WHERE nota >= (SELECT ROUND(AVG(nota), 2) FROM alugueis);
+
+
+
+
+
+# =======           PARTE 4:             =======#
+# =======  Criando Views - CREATE VIEW   =======#
+
+
+-- CREATE/DROP VIEW: Guardando o resultado de uma consulta no nosso banco de dados
+
+
+-- CASE 5. Crie uma view para guardar o resultado do SELECT abaixo.
+
+-- CREATE VIEW === PESQUISAR
+CREATE VIEW resultado AS
+SELECT
+	genero,
+	ROUND(AVG(preco_aluguel), 2) AS media_preco,
+    COUNT(*) AS qtd_filmes
+FROM filmes
+GROUP BY genero;
+
+SELECT * FROM resultado;
+
